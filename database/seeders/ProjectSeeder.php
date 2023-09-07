45aa416a64b4f8fb7ce2011651d21a5f31990270
carloeusebi <carloeusebi@gmail.com>
2023-09-07 14:11:37 +0200

    Updated ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -16,12 +17,16 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker): void
     {
         $DIR = 'project_images';
-
         Storage::makeDirectory($DIR);
+
+        $typeIds = Type::pluck('id')->toArray();
+        $typeIds[] = null; //add a null element to have some Project without a relation
+
         // creates 10 records
         for ($i = 0; $i < 10; $i++) {
             Project::create([
                 'name' => $faker->words(3, true),
+                'type_id' => $typeIds[array_rand($typeIds)],
                 'thumbnail' => "$DIR/" . $faker->image(storage_path("app/public/$DIR"), 250, 250, fullPath: false),
                 'description' => $faker->paragraph(10),
                 'url' => $faker->url(),
